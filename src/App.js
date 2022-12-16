3d9b3d5df24b8199e00f7dd9ce1272a3fd3b78f2
Dalton Carter <DJ_Carter_Gottenbos@yahoo.com>
2022-12-15 16:52:28 -0800

    unit project complete.

--- src/App.js
+++ src/App.js
@@ -14,12 +14,35 @@ function App() {
   setPlayer(true)
  }
 
+const calculateWinner = (arr) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i]
+    if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+      return `${arr[a]} won!`
+    }
+  }
+  return "Who will win?"
+}
+
   return (
     <div className="App">
+      <span>{calculateWinner(squares)}</span>
       <div className='container'>
         {squares.map((val, index) =>{
             return(
               <Square
+                        key={index}
                         setSquares={setSquares}
                         index={index}
                         squareValue={val}
@@ -30,6 +53,7 @@ function App() {
             )
         })}
       </div>
+      
         <button onClick={handleClick}>Reset</button>
     </div>
   );
